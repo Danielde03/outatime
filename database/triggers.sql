1c878eb184ccc8b129dbc49de3b443048b56fff4
Danielde03 <porter.42.de@gmail.com>
2023-03-08 16:16:50 -0700

    fixed trigger

--- database/triggers.sql
+++ database/triggers.sql
@@ -3,8 +3,8 @@ CREATE OR REPLACE FUNCTION outatime.make_user_page_func() RETURNS trigger
    LANGUAGE plpgsql AS
 $$BEGIN
    
-    INSERT INTO outatime.user_page (user_id, banner)
-        VALUES (NEW.user_id, ' ');
+    INSERT INTO outatime.user_page (user_id, "aboutUs", banner)
+        VALUES (NEW.user_id, ' ', ' ');
 
     RETURN NEW;
 
